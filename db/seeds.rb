e0e51525606aa724f4939bf62bebf60a3cbb9b87
Annah <annah.gerletti@fluxfederation.com>
2018-08-11 16:30:17 +1200

    working cats website
    seeded the data base with 2 photos
    rendering them on the page

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,11 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+  # movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+  #  Character.create(name: 'Luke', movie: movies.first)
+
+   photos = Photo.create([{url: "https://i.ytimg.com/vi/wBEFlFwVyRE/hqdefault.jpg"}])
+   photos = Photo.create([{url: "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fpeopledotcom.files.wordpress.com%2F2018%2F04%2Floki_the_sphynx-01_1.jpg%3Fw%3D1800&w=700&q=85"}])
+
+
+
